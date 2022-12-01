227dd01a2569c39d4a781c1709cd29fd51ca7b2a
joao garcia <jiboiadamor@gmail.com>
2022-12-01 12:02:28 +0000

    button play with some errors

--- player-bundle.js
+++ player-bundle.js
@@ -307,10 +307,10 @@ module.exports = SoundCloud;
 const SoundCloudAudio = require('soundcloud-audio');
 const player = new SoundCloudAudio('95f22ed54a5c297b1c41f72d713623ef');
 
-window.play = () => {
-  player.resolve("https://soundcloud.com/biskwiq/dawntheopus", (track) => {
-    player.play();
-  });
+window.play = function () {
+    player.resolve("https://soundcloud.com/biskwiq/dawntheopus", function (track) {
+        player.play();
+    });
 }
 
 },{"soundcloud-audio":1}]},{},[2]);
